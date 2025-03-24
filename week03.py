0fe6d3799a6290c0a3a13449ee3ea81e4c9104f8
UnderCruzer <mj030725@gmail.com>
2025-03-24 17:52:24 +0900

    v0.6.0

--- week03.py
+++ week03.py
@@ -1,5 +1,6 @@
-groups = ['HOT', 'seventeen', 'blackpink', 'njz']
-# ratings = [1, 2, 4, 3, 100]
-ratings = [1, 2, 4, 3]
-group_rating = list(zip(groups, ratings))
-print(group_rating)
+cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
+cities = set(cities)
+cities.add('Incheon')
+cities.add('Suwon')
+# 딕셔너리와 리스트는 순서를 갖지 않는다. (중복값은 존재하지 않음) -> 딕셔너리의 키 값만 존재하는것을 set
+print(cities)
