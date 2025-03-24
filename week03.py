a81a5bf27259dbf196edc56b63994180e6ee8b19
UnderCruzer <mj030725@gmail.com>
2025-03-24 18:06:09 +0900

    v1.0 array

--- week03.py
+++ week03.py
@@ -1,24 +1,10 @@
-def duplicate_city(cities):
-    result_city = []
-    s = set()
+def inters(l1,l2):
+    l3 = []
+    for v in l1 :
+        if v in l2:
+            l3.append(v)
+    return l3
 
-    for city in cities:
-        l1 = len(s)
-        s.add(city)
-        l2 = len(s)
-        if l1 == l2:
-            result_city.append(city)
-    return result_city
-
-
-
-cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
-#cities = set(cities)
-#cities.add('Incheon')
-#cities.add('Suwon')
-cities.append('Incheon')
-cities.append('Suwon')
-cities.append('Seoul')
-# 딕셔너리와 리스트는 순서를 갖지 않는다. (중복값은 존재하지 않음) -> 딕셔너리의 키 값만 존재하는것을 set
-print(cities)
-print(set(duplicate_city(cities))) #중복값찾기
+l1 = [45, 5, 22, 31, 7, 19]
+l2 = [22, 1, 5, 2, 7, 28, 27, 19, 13, 41]
+print(inters(l1,l2))
