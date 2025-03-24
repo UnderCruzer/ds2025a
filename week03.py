5f02c20cad1092b2a2906aa174f42b23a2de6ef2
UnderCruzer <mj030725@gmail.com>
2025-03-24 17:58:16 +0900

    v0.9 array

--- week03.py
+++ week03.py
@@ -1,6 +1,24 @@
+def duplicate_city(cities):
+    result_city = []
+    s = set()
+
+    for city in cities:
+        l1 = len(s)
+        s.add(city)
+        l2 = len(s)
+        if l1 == l2:
+            result_city.append(city)
+    return result_city
+
+
+
 cities = ['Incheon', 'Seoul', 'Incheon', 'Incheon', 'Gwangju']
-cities = set(cities)
-cities.add('Incheon')
-cities.add('Suwon')
+#cities = set(cities)
+#cities.add('Incheon')
+#cities.add('Suwon')
+cities.append('Incheon')
+cities.append('Suwon')
+cities.append('Seoul')
 # 딕셔너리와 리스트는 순서를 갖지 않는다. (중복값은 존재하지 않음) -> 딕셔너리의 키 값만 존재하는것을 set
 print(cities)
+print(set(duplicate_city(cities))) #중복값찾기
