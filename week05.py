070839caeba28eb81bd5b60de9e58238e7a98c15
UnderCruzer <mj030725@gmail.com>
2025-04-07 18:17:35 +0900

    v2.4 stack

--- week05.py
+++ week05.py
@@ -1,10 +1,18 @@
-s1 = []
-s1.append("Data structure")
-s1.append("DataBase")
-print(s1[-1])
-print(s1)
-print(s1.pop())
-print(s1)
-print(s1.pop())
-print(s1)
-
+# print(1+2))
+def is_valid_parentheses(expression : str) -> bool:
+    # str 매개변수 데이터 타입 . bool 리턴 타입
+    stack = list()
+    for letter in expression:
+        if letter == "(":
+            stack.append(letter)
+        if letter == ")":
+            if len(stack) == 0:
+                return False
+            else:
+                stack.pop()
+    return len(stack) == 0
+# 여는 괄호가 더 많을 때 False
+print(is_valid_parentheses(")1+2())"))
+print(is_valid_parentheses("(1+2))"))
+print(is_valid_parentheses("(1+2)"))
+print(is_valid_parentheses("((1+2)/2)"))
