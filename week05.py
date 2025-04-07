7e5e9f10a44b0dcb05a506648e2129725404de81
UnderCruzer <mj030725@gmail.com>
2025-04-07 17:38:44 +0900

    v2.1 stack 2

--- week05.py
+++ week05.py
@@ -1,12 +1,34 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.link = None
+
+
 class Stack:
     def __init__(self):
         self.items = list()
+        self.top = None
 
     def push(self, item):
         self.items.append(item)
 
+    def push(self, data):
+        node = Node(data)
+        if self.top is None:
+            self.top = node
+        else:
+            node.link = self.top
+            self.top = node  # top update
+
     def pop(self):
-        return self.items.pop()
+        if self.top is None:
+           # raise IndexError("스택이 비어있습니다")
+            return "stack is empty"
+        popped_node = self.top
+        self.top = self.top.link
+        popped_node.link = None
+        return popped_node.data
+
 
     def size(self):
         return len(self.items)
@@ -16,18 +38,16 @@ class Stack:
 
     def peek(self):
         return self.items[-1]
+        if self.top is None:
+            return "Stack is empty!"
+        popped_node = self.top
+        self.top = self.top.link
+        return popped_node.data
 
 
 s1 = Stack()
 s2 = Stack()
-print(s1.is_empty())
 s1.push("Data structure")
-print(s1.is_empty())
-print(s2.is_empty())
 s1.push("Database")
-print(s1.size())
-print(s1.peek())
-print(s1.size())
 print(s1.pop())
-print(s1.size())
-print(s1.peek())
+print(s1.pop())
